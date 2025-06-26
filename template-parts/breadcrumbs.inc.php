016ec537cbcfc3399427c7afec3e36d3a4655730
JoaoReisSilva <81335231+JoaoReisSilva@users.noreply.github.com>
2025-06-26 10:32:25 +0000

    test

--- template-parts/breadcrumbs.inc.php
+++
@@ -1,8 +0,0 @@
-<div
-  id="breadcrumbs"
-  class="breadcrumbs wrapper"
-  itemscope=""
-  itemtype="http://schema.org/BreadcrumbList"
->
-    <?php the_breadcrumbs(); ?>
-</div>
--- template-parts/header/logo.inc.php
+++
@@ -1,4 +0,0 @@
-<a href="<?php echo home_url(); ?>" class="header-logo">
-  <span class="header-logo__title"><?php echo get_theme_mod( 'header_logo_title' ); ?></span>
-  <span class="header-logo__subtitle"><?php echo get_theme_mod( 'header_logo_subtitle' ); ?></span>
-</a>
